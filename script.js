8e3002c77d8e78bb1460cb621a70bf8a82bf207c
reeceawalsh <reeceawalsh@hotmail.co.uk>
2021-08-10 23:02:42 +0100

    Added popup open and close buttons and functionality
    Need to write the functions for adding books tomorrow

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Assigning buttons
+const popUpBtn = document.getElementById("popUpBtn");
+const bookInputForm = document.getElementById("bookInputForm");
+const sendEmailBtn = document.getElementById("sendEmailBtn");
+const closeFormBtn = document.getElementById("closeFormBtn");
+const addBookBtn = document.getElementById("addBookBtn");
+sendEmailBtn.addEventListener("click", console.log("working1"));
+
+popUpBtn.onclick = function () {
+  bookInputForm.classList.toggle("hide");
+};
+
+closeFormBtn.onclick = function () {
+  bookInputForm.classList.toggle("hide");
+};
+
+// After the user has filled out all of the information and clicked the addBookBtn, a new grid with all of the relevant information will be created and the object will be saved in a database.
+
+// Adding books functions
+
 // let myLibrary = [harryPotter, lordOfTheRings];
 
 // class Book {
